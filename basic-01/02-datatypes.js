166e9917330fdee6be7cdca4107f86f34f8bb1af
02atul <165402423+02atul@users.noreply.github.com>
2024-03-30 03:18:04 +0000

    datatypes

---
+++ basic-01/02-datatypes.js
@@ -0,0 +1,12 @@
+let name  = "Atul chakwa";
+let age = 65;
+let isLoggedIn = true;
+ let char = 'c';
+ 
+ 
+ console.log(typeof name)
+ console.log(typeof age)
+ console.log(typeof isLoggedIn)
+ console.log(typeof char)
+
+ //type of data use in javascript typeof is function
