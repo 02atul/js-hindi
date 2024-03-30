aba7eeb2e34947fc8f9d426b8a9d8efb68427246
02atul <165402423+02atul@users.noreply.github.com>
2024-03-30 03:13:09 +0000

    variable

---
+++ basic-01/01_variable.js
@@ -0,0 +1,6 @@
+const accountId = 123456;
+let accountEmail = "atul@acropolis.in";
+let accounPassword = "98765";
+accountCity = " Jabalpur"
+console.log(typeof accountEmail);
+console.table([accountId,accountEmail,accounPassword,accountCity]);
