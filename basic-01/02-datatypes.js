6eeff499ca44197aa4784e34cc52de2a53ebb7a4
02atul <165402423+02atul@users.noreply.github.com>
2024-03-30 06:27:23 +0000

    conversion

--- basic-01/02-datatypes.js
+++ basic-01/02-datatypes.js
@@ -4,9 +4,9 @@ let isLoggedIn = true;
  let char = 'c';
  
  
- console.log(typeof name)
- console.log(typeof age)
- console.log(typeof isLoggedIn)
- console.log(typeof char)
+ console.log(typeof (name))
+ console.log(typeof (age))
+ console.log(typeof (isLoggedIn))
+ console.log(typeof (char))
 
  //type of data use in javascript typeof is function
---
+++ basic-01/03-conversion.js
@@ -0,0 +1,21 @@
+let score = 33;
+
+ console.log(typeof(score));
+ console.log(score);
+
+  let value = String(score)
+   console.log(typeof(value));
+   console.log(value);
+
+
+
+   let str = "stsgvkcabs";
+    
+    console.log(typeof(str));
+    console.log(str);
+
+      let value2 = Number(str)
+      console.log(typeof(value2));
+      console.log(value2);
+
+       let 
