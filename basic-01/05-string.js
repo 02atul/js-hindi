c5218cb2254b66043bbf203c1dda7c311d2fc3e1
02atul <165402423+02atul@users.noreply.github.com>
2024-04-02 15:28:03 +0000

    string

---
+++ basic-01/05-string.js
@@ -0,0 +1,12 @@
+let name = "    atul    ";
+console.log(name.trim())
+console.log(name)
+
+ let str = "asdfghjklqwertyuiop@gmail.com"
+
+ console.log(str.substring(2,8));
+ console.log(str.slice(2,4));
+ console.log(str.replace('@','--'))
+ console.log(str.split('l'))
+
+//  read string function
