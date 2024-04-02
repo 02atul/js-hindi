0ac1300795984e1c66da39da161d37d04975c67c
02atul <165402423+02atul@users.noreply.github.com>
2024-04-02 15:27:48 +0000

    comaparison

---
+++ basic-01/04-comparison.js
@@ -0,0 +1,12 @@
+
+console.log("2"==2);
+
+// output true
+
+console.log("2"===2);
+// false because (===)(strict uses)  check datatypes
+
+ console.log(null>0)
+ console.log(null==0)
+ console.log(null>=0)
+
