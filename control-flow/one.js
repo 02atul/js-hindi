7c1457b5b3c9851fa0643db1e2e2f07b16b57c92
02atul <165402423+02atul@users.noreply.github.com>
2024-04-13 04:48:56 +0000

    if

---
+++ control-flow/one.js
@@ -0,0 +1,34 @@
+// if
+
+// if(2=="2"){
+//     console.log("excute");
+// }
+// if(2>=2){
+//         console.log("excute");
+//     }
+// if(2==="2"){
+//          console.log("excute");
+//      }
+//      else
+//      {
+//         console.log("not excute");
+//      }
+
+// if(-1<-3){
+//         console.log("excute");
+//     }
+
+const userloggedInGoogle = true;
+const userloggedInemail = false;
+
+// if(userloggedInGoogle || userloggedInemail){
+//     console.log("excute");
+// }
+
+// if(userloggedInGoogle &&  userloggedInemail){
+//     console.log("excute");
+// }
+if(2=="2")  console.log("excute"), console.log("excute2");
+if(2=="2")  console.log("excute3");
+             
+         
